af09e8dc491577203898bce63bc546c9a129d07b
Rodrigo Will <44774584+RodrigoWillX@users.noreply.github.com>
2018-11-16 09:36:20 -0200

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Atiradores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    class Atiradores
+    {
+        protected string nome;
+        protected int dano, distancia, alcanco, municao, vida;
+
+        public Atiradores(string DefineNome, int DefineDano, int DefineDistancia, int DefineMunicao, int DefineVida, int DefineAlcanco)
+        {
+            this.nome = DefineNome;
+            this.dano = DefineDano;
+            this.distancia = DefineDistancia;
+            this.municao = DefineMunicao;
+            this.vida = DefineVida;
+            this.alcanco = DefineAlcanco;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atiradores j1 = new Atiradores("Dimitri", 20, 6, 100, 50, 4);
+            Atiradores j2 = new Atiradores("Nikolai", 10, 6, 30, 100, 2);
+            Atiradores j3 = new Atiradores("Mikhail", 40, 6, 1, 35, 5);
+            Atiradores j4 = new Atiradores("Bóris", 20, 6, 100, 50, 4);
+            Atiradores j5 = new Atiradores("Serguei", 10, 6, 30, 100, 2);
+            Atiradores j6 = new Atiradores("Samiro", 40, 6, 1, 35, 5);
+        }
+    }
+}
